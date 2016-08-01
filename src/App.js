e5e7438d2b38c97dc83c7dc4057148a9790b9443
Vintizer <vizerkharkiv@gmail.com>
2016-08-01 20:15:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import $ from 'jQuery';
+
+export default class App extends Component {
+
+	render() {
+		return (
+			<blockquote>
+				<h1 className="">Hello, world.</h1>	
+			</blockquote>
+		);
+	}
+}
